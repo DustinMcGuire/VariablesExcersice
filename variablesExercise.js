044825cf046ead5d6bf58fd8ecab20bd9d6ef419
Dustin McGuire <dustinmcguire@KCCCs-MacBook-Pro-3.local>
2021-02-23 13:42:53 -0500

    completed variablesExercise

---
+++ variablesExercise.js
@@ -0,0 +1,31 @@
+// 1
+// const
+// 2
+let num = 20;
+console.log(num);
+// 3
+num = 15;
+console.log(num);
+// 4
+num = num + 7
+console.log(num);
+// 5
+num--;
+console.log(num);
+// 6
+num = "this is not a number"
+console.log(num);
+// 7
+let blank;
+console.log(blank);
+// 8 & 9
+blank = null
+console.log(blank);
+// Bonus
+// 1
+const quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01;
+// 2
+console.log(quarter, dime, nickel, penny);
+// 3
+let letters = "qwertywiop"
+console.log(letters [letters.length -1]);
